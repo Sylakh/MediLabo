29d841a9290e5944c239bafd14d5fc25b28f9c82
Sylakh <sylvainsds@gmail.com>
2024-07-16 16:50:28 +0200

    initial commit sprint 1

---
+++ microback/src/test/java/com/openclassrooms/microback/MicrobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.microback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
