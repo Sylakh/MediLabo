28c3e2386eea04da9b5887ba8fa283ffe4f106f2
Sylakh <sylvainsds@gmail.com>
2024-07-28 18:01:16 +0200

    eureka server + discovery on microback

--- microback/src/main/java/com/openclassrooms/microback/MicrobackApplication.java
+++ microback/src/main/java/com/openclassrooms/microback/MicrobackApplication.java
@@ -2,8 +2,10 @@ package com.openclassrooms.microback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 // @EnableSwagger2
 public class MicrobackApplication {
 
